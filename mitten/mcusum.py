2ec8fec0e6910e1832f67df247008881d4b94cae
devinsohi <devinsohi@gmail.com>
2020-12-19 20:15:07 -0500

    Update mcusum.py
    there is once again plotting included in mcusum

--- mitten/mcusum.py
+++ mitten/mcusum.py
@@ -1,9 +1,7 @@
 import numpy as np
 from numpy import matmul, sqrt, array
 
-def mcusum(df,
-		   num_in_control,
-		   k,):
+def mcusum(df, num_in_control,k,alpha=.01, plotting=True, save="", plot_title="MCUSUM"):
 	"""
 	Implementation of the Multivariate Cumulative Sum (MCUSUM) method.
 
@@ -17,6 +15,9 @@ def mcusum(df,
 		df: multivariate dataset as Pandas DataFrame
 		num_in_control: number of in control observations
 		k: the slack parameter which determines model sensetivity (should typically be set to 1/2 of the mean shift that you expect to detect)
+		alpha: the percentage of false positives we want to allow, used for calculating the Upper Control Limit (default .01 or 1%)
+		save: the directory to save the graphs to, if not changed from default, nothing will be saved
+		plot_title: the title for the plot generated
 	Returns:
 		MCUSUM statistic values as a list
 	"""
@@ -46,6 +47,38 @@ def mcusum(df,
 			y = matmul(matmul(tf, cov_inv), tf)
 			y = sqrt(y)
 			y_vals[n] = y
+	
+	#calculate UCL
+	in_stats = y_vals[0:num_in_control]
+  	ucl = max(in_stats)
+ 
+  	count = len([i for i in in_stats if i > ucl]) 
 
-	#returns the mcusum statistics as a list
-	return y_vals
+  	while(count < (alpha* len(in_stats))):
+      		ucl = ucl - step_size
+     		count = len([i for i in in_stats if i > ucl])
+	
+	#plotting
+	if plotting:
+		fig, ax = plt.subplots(figsize=(10, 7))
+		lc = threshold_plot(ax, range(0, df.shape[0]), array(y_vals), ucl, 'b', 'r')
+		ax.axhline(ucl, color='k', ls='--')
+		ax.set_title(plot_title)
+		ax.set_xlabel('Observation Number')
+		ax.set_ylabel('MCUSUM statistic (Anomaly Score)')
+
+		#saving plot
+		if save != "":
+			if save[-1] != '/':
+				save += '/'
+			plt.savefig(save + plot_title + '_FULL.png', dpi=300)
+			
+			#if there are a lot of entries, a smaller graph will also be saved that
+			#shows only the end of the graph for more detail.
+			if len(y_vals) > 10000:
+				ax.set_xlim(9000)
+				plt.savefig(save + plot_title + '_SMALL.png', dpi=300)
+			
+		
+	#returns the mcusum statistics as a list and upper control limit
+	return y_vals, ucl
