2f5d52bf577b97403a48e1e1ae2a2228657bb573
devinsohi <devinsohi@gmail.com>
2020-12-19 20:22:45 -0500

    Update hotelling.py
    Hotellings now has plotting again

--- mitten/hotelling.py
+++ mitten/hotelling.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pandas as pd
 
-def hotelling_t2(df,
-                 num_in_control):
+def hotelling_t2(df, num_in_control, alpha=.01, plotting=True, save="", plot_title="Hotellings"):
     """
     Args:
         df: multivariate dataset as Pandas DataFrame
         num_in_control: number of in control observations before the anomalies start
+        alpha: the percentage of false positives we want to allow, used for calculating the Upper Control Limit (default .01 or 1%)
+		    save: the directory to save the graphs to, if not changed from default, nothing will be saved
+		    plot_title: the title for the plot generated
     Returns:
-        t^2 statistic values
+        t^2 statistic values and the UCL
     """
     # calculate covariance matrix
     S = df.head(num_in_control).cov()
@@ -23,6 +25,37 @@ def hotelling_t2(df,
         t2 = diff.transpose() @ s_inv
         t2 = t2@diff
         t2_values.append(t2)
+       
+    #calculate UCL
+	  in_stats = t2_vals[0:num_in_control]
+  	ucl = max(in_stats)
+ 
+  	count = len([i for i in in_stats if i > ucl]) 
+
+  	while(count < (alpha* len(in_stats))):
+        ucl = ucl - step_size
+        count = len([i for i in in_stats if i > ucl])  
+      
+    #plotting
+	  if plotting:
+        fig, ax = plt.subplots(figsize=(10, 7))
+        lc = threshold_plot(ax, range(0, df.shape[0]), array(y_vals), ucl, 'b', 'r')
+        ax.axhline(ucl, color='k', ls='--')
+        ax.set_title(plot_title)
+        ax.set_xlabel('Observation Number')
+        ax.set_ylabel('Hotelling statistic (Anomaly Score)')
+
+        #saving plot
+        if save != "":
+            if save[-1] != '/':
+                save += '/'
+            plt.savefig(save + plot_title + '_FULL.png', dpi=300)
+
+            #if there are a lot of entries, a smaller graph will also be saved that
+            #shows only the end of the graph for more detail.
+            if len(y_vals) > 10000:
+                ax.set_xlim(9000)
+                plt.savefig(save + plot_title + '_SMALL.png', dpi=300)
 
     # return t2 values
-    return t2_values
+    return t2_values, ucl
